02664a127b21f254d3d15366e68b233328053c94
olive <pujaasuresh@gmail.com>
2021-08-09 16:17:46 +0530

    Updates

---
+++ precache-manifest.8c4c9445ddd1d4e56430ba5e386f2d89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6000dd6e143b50dfcce6ca13ff42a4a2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea3fdff49217eae94bae",
+    "url": "./static/css/2.cd7f2e05.chunk.css"
+  },
+  {
+    "revision": "3ba9ae617de507df2424",
+    "url": "./static/css/main.1c3ccaaf.chunk.css"
+  },
+  {
+    "revision": "ea3fdff49217eae94bae",
+    "url": "./static/js/2.accea084.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.accea084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba9ae617de507df2424",
+    "url": "./static/js/main.e55a2bf5.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  },
+  {
+    "revision": "834a9454f02370b5dad88fa7601f91d3",
+    "url": "./static/media/pujaa_cv.834a9454.pdf"
+  },
+  {
+    "revision": "51db75ee782da859e119544097980778",
+    "url": "./static/media/work-image.51db75ee.png"
+  }
+]);
